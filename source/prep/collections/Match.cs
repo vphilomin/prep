9ef5ecc5d5e1c86555f877f1a441c8f4ef67bd8c
Roman Razilov <roman.razilov@philips.com>
2014-08-11 14:41:13 +0200

    Pushing new changes

--- source/prep/collections/Match.cs
+++ source/prep/collections/Match.cs
@@ -1,7 +1,27 @@
-﻿namespace prep.collections
+﻿using System;
+using prep.matching;
+
+namespace prep.collections
 {
-  public class Match
-  {
-     
-  }
+	public class Match<ItemForMatch>
+	{
+		public static MatchForAttribute<ItemForMatch, AtrributeType> with_attribute<AtrributeType>(
+			Func<ItemForMatch, AtrributeType> attributeGetter)
+		{
+			return new MatchForAttribute<ItemForMatch, AtrributeType>(attributeGetter);
+		}
+	}
+
+
+	public delegate AtrributeType MatchForAttribute<in ItemForMatch, out AtrributeType>(ItemForMatch item);
+
+	public static class MatchExtentions
+	{
+		public static IMatchA<ItemToMatch> equal_to<ItemToMatch, AtrributeType>(
+			this MatchForAttribute<ItemToMatch, AtrributeType> attribute,
+			AtrributeType value)
+		{
+			return new ConditionalSpecification<ItemToMatch>(m => object.Equals(m, value));
+		}
+	}
 }
