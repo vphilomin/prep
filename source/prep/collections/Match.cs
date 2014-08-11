94f886e1b380408d3f54eb8d75d94f1881b4d39c
Roman Razilov <roman.razilov@philips.com>
2014-08-11 15:06:10 +0200

    Pushing new changes

--- source/prep/collections/Match.cs
+++ source/prep/collections/Match.cs
@@ -4,11 +4,26 @@ namespace prep.collections
 {
   public class Match<ItemForMatch>
   {
-    public static IGetTheValueOfAnAttribute<ItemForMatch, AttributeType> 
+    public static AttributeAccessor<ItemForMatch, AttributeType> 
       with_attribute<AttributeType>(IGetTheValueOfAnAttribute<ItemForMatch, AttributeType> accessor)
     {
-      return accessor;
+		return new AttributeAccessor<ItemForMatch, AttributeType>(accessor);
     }
   }
+
+	public class AttributeAccessor<ItemToRetrieveAttributeFrom, AttributeType>
+	{
+		private IGetTheValueOfAnAttribute<ItemToRetrieveAttributeFrom, AttributeType> accessor;
+
+		public AttributeAccessor(IGetTheValueOfAnAttribute<ItemToRetrieveAttributeFrom, AttributeType> accessor)
+		{
+			this.accessor = accessor;
+		}
+
+		public IMatchA<ItemToRetrieveAttributeFrom> equal_to(AttributeType value)
+		{
+			return new ConditionalSpecification<ItemToRetrieveAttributeFrom>(x => accessor(x).Equals(value));
+		} 
+	}
 
 }
