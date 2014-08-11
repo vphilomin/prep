d20225b4c7727086b23a2c3b001b1c080061a7a4
vphilomin <vphilomin@gmail.com>
2014-08-11 11:35:15 +0200

    Pushing new changes

--- source/prep/collections/Movie.cs
+++ source/prep/collections/Movie.cs
@@ -2,12 +2,25 @@ using System;
 
 namespace prep.collections
 {
-  public class Movie
+  public class Movie : IEquatable<Movie>
   {
     public string title { get; set; }
     public ProductionStudio production_studio { get; set; }
     public Genre genre { get; set; }
     public int rating { get; set; }
     public DateTime date_published { get; set; }
+      public bool Equals(Movie other)
+      {
+          if (other == null) return false;
+
+          if (ReferenceEquals(this, other)) return true;
+
+          return title == other.title;
+      }
+
+      public override bool Equals(object obj)
+      {
+          return Equals(obj as Movie);
+      }
   }
 }
